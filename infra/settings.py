a88d7ad09f010b6ea526363dd8b34ad75476f12b
Паутов Александр <sh2d000w@gmail.com>
2025-03-03 17:29:00 +0300

    fix: Добавил драйвер в настройки.

--- infra/settings.py
+++ infra/settings.py
@@ -1,23 +1,26 @@
 from typing import Any
 
 from app.dummy.counter import DummyCounter
+from app.owen_counter.owen_ci8 import OwenCI8
 
 serial_settings: dict[str, Any] = {
     # 'port': '/dev/ttyUSB0',
-    # 'baudrate': 9600,
-    # 'bytesize': 8,
-    # 'parity': 'N',
-    # 'stopbits': 1,
-    # 'timeout': 0.2
+    'port': '/dev/pts/4',
+    'baudrate': 9600,
+    'bytesize': 8,
+    'parity': 'N',
+    'stopbits': 1,
+    'timeout': 0.2
 }
 
 sensors_settings: list[dict[str, Any]] = [
-    # {
-    #     'name': 's10',
-    #     'addr': 1,
-    #     'addr_len': 8,
-    #     'parameter': OwenCI8.DCNT
-    # },
+    {
+        'name': 's10',
+        'driver': OwenCI8,
+        'addr': 1,
+        'addr_len': 8,
+        'parameter': OwenCI8.DCNT
+    },
     # {
     #     'name': 's11',
     #     'addr': 2,
@@ -38,9 +41,10 @@ sensors_settings: list[dict[str, Any]] = [
     # },
     {
         'name': 'test1',
-        'addr': 0,
-        'addr_len': 0,
-        'parameter': DummyCounter.TEST
+        'driver': DummyCounter,
+        'addr': None,
+        'addr_len': None,
+        'parameter': None
     },
 ]
 
